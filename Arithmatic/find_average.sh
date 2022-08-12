402e9c5f89334c56ecb84650d8cdacc82d0e288d
rebeccabartels <beccacode@gmail.com>
2022-08-11 23:05:28 -0400

    find average script for grades

---
+++ Arithmatic/find_average.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+count=0;
+total=0; 
+
+cat grades_module_1_82022.csv | awk -F "," ' {print $2, $7}' > ./test.csv
+cat test.csv | awk -F " " ' {print $2}' > ./nums.csv
+cat nums.csv | awk '{ total += $1; count++ } END { print total/count }' nums.csv
