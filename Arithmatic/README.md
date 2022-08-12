da297fb89cbf6d348474b225abc7ae3e25717945
rebeccabartels <beccacode@gmail.com>
2022-08-12 00:11:21 -0400

    added tail command to fix the fact that the script was adding the header values and an empty row....

--- Arithmatic/README.md
+++ Arithmatic/README.md
@@ -1,1 +1,2 @@
 Convenient scripts to make everyday calculations more convenient.
+removed grades document with identifying information and just kept the numbers
--- Arithmatic/find_average.sh
+++ Arithmatic/find_average.sh
@@ -3,5 +3,5 @@ count=0;
 total=0; 
 
 cat grades_module_1_82022.csv | awk -F "," ' {print $2, $7}' > ./test.csv
-cat test.csv | awk -F " " ' {print $2}' > ./nums.csv
+cat test.csv | awk -F " " ' {print $2}'| tail -n+3 > ./nums.csv
 cat nums.csv | awk '{ total += $1; count++ } END { print total/count }' nums.csv
